b350592960f65983a89cb46c5e72927e7304f647
Nikita <naberdyev@gmail.com>
2021-08-14 04:44:57 +0400

    add mobile menu page

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
     var hamburger = $('#hamburger-icon');
     hamburger.click(function() {
        hamburger.toggleClass('active');
+       $('.mobile-menu').toggleClass('mobile-menu_active');
+       $('.body').toggleClass('body_inactive');
        return false;
     });
   });
+  
