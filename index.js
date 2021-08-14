457c87e446e1690e733f0ff99b6726f12e1d4f9e
Nikita <naberdyev@gmail.com>
2021-08-14 05:25:27 +0400

    add functionality to the mobile-menu

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 $( document ).ready(function() {
-    var hamburger = $('#hamburger-icon');
-    hamburger.click(function() {
-       hamburger.toggleClass('active');
-       $('.mobile-menu').toggleClass('mobile-menu_active');
-       $('.body').toggleClass('body_inactive');
+    $('#hamburger-icon').click(function() {
+       $('#hamburger-icon').toggleClass('active')
+       $('.mobile-menu').toggleClass('mobile-menu_active')
+       $('.body').toggleClass('body_inactive')
        return false;
     });
   });
   
+$(document).ready(function () {
+    $('.mobile-menu__link').click(function (e) { 
+        $('.body').toggleClass('body_inactive')
+        $('.mobile-menu').toggleClass('mobile-menu_active')
+        $('#hamburger-icon').toggleClass('active')
+    });
+});
