26089762cf6e9b236c8d29566163a674e8eea890
Nikita <naberdyev@gmail.com>
2021-08-14 04:07:31 +0400

    add burger icon and animation

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// const { src } = require("gulp");
-
-// function changeOverviewImage() {
-//     if ($('.overview__bottom-block').width() < 800) {
-//         $('img.overview__image').attr("src", "img/overview/2.png");
-//     }
-//     else $('img.overview__image').attr("src", "img/overview/1.png");
-// }
-
-// changeOverviewImage()
-
-// $( window ).resize(changeOverviewImage);
+$( document ).ready(function() {
+    var hamburger = $('#hamburger-icon');
+    hamburger.click(function() {
+       hamburger.toggleClass('active');
+       return false;
+    });
+  });
