06e5af9a663b8e8f949f16ef4a1fd4b269a37d54
Nikita <naberdyev@gmail.com>
2021-08-14 19:47:19 +0400

    add change-language menu to mobile menu page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
     $('#hamburger-icon').click(function() {
        $('#hamburger-icon').toggleClass('active')
        $('.mobile-menu').toggleClass('mobile-menu_active')
+       $('.mobile-menu__change-language').toggleClass('change-language_active');
        $('.body').toggleClass('body_inactive')
        return false;
     });
@@ -11,6 +12,7 @@ $(document).ready(function () {
     $('.mobile-menu__link').click(function (e) { 
         $('.body').toggleClass('body_inactive')
         $('.mobile-menu').toggleClass('mobile-menu_active')
+        $('.mobile-menu__change-language').toggleClass('change-language_active');
         $('#hamburger-icon').toggleClass('active')
     });
 });
